b386517dab2adba7e9816608164c9633196f21c7
Chuanqi Zhang <zhchqi@gmail.com>
2022-08-05 09:40:26 -0400

    init

---
+++ 007_book_on-java-8/source_code/OCP-17-Study-Guide/src/main/java/ch04/Ch04.java
@@ -0,0 +1,16 @@
+package ch04;
+
+import org.junit.jupiter.api.Test;
+
+public class Ch04 {
+
+    @Test
+    void test01() {
+        System.out.println("hello!");
+        System.out.println("test01");
+    }
+
+    void test02() {
+        System.out.println("test02");
+    }
+}
