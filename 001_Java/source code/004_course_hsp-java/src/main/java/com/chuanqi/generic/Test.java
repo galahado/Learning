c3bf06c9d02d8abf8dd0203e68b3bcbf5393c811
Chuanqi Zhang <zhang.chua@northeastern.edu>
2022-09-26 17:49:18 -0400

    Delete 001_Java/source code/004_course_hsp-java directory

--- 001_Java/source code/004_course_hsp-java/src/main/java/com/chuanqi/generic/Test.java
+++
@@ -1,9 +0,0 @@
-package com.chuanqi.generic;
-
-
-public class Test {
-    public static void main(String[] args) throws ClassNotFoundException {
-        String s = "java.lang.String";
-        Class c = Class.forName(s);
-    }
-}
