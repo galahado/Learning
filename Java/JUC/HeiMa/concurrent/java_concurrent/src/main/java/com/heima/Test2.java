58167ef4f6015e026d24b5e7fb6b034735ee587b
Chuanqi Zhang <zhchqi@gmail.com>
2022-12-30 19:45:32 -0500

    spring_01_quickstart

---
+++ Java/JUC/HeiMa/concurrent/java_concurrent/src/main/java/com/heima/Test2.java
@@ -0,0 +1,22 @@
+package com.heima;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+@Slf4j(topic = "c.Test2")
+public class Test2 {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        FutureTask<Integer> task3 = new FutureTask<>(() -> {
+            log.debug("running...");
+            return 100;
+        });
+
+        new Thread(task3, "t3").start();
+
+        Integer result = task3.get();
+        log.debug("结果是:{}", result);
+    }
+}
