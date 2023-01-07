38aaedc7642dfc3cac6fc32a0b58de6a2b460e28
Chuanqi Zhang <zhchqi@gmail.com>
2023-01-06 20:31:50 -0500

    mybatis_01_helloworld

---
+++ Java/SSM/AtGuiGu/Project/SSM/mybatis_01_helloworld/src/test/java/com/atguigu/mybatis/test/MyBatisTest.java
@@ -0,0 +1,35 @@
+package com.atguigu.mybatis.test;
+
+import com.atguigu.mybatis.mapper.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyBatisTest {
+
+    @Test
+    public void test() throws IOException {
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        int result = mapper.insertUser();
+
+        System.out.println(result);
+
+        sqlSession.commit();
+
+        sqlSession.close();
+    }
+}
