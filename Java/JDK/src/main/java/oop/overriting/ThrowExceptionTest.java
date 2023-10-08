3a5ee7e4d4f11644803147e194594b88d038d315
Chuanqi Zhang <zhchqi@gmail.com>
2023-10-08 14:25:19 -0400

    add ThrowExceptionTest.java

---
+++ Java/JDK/src/main/java/oop/overriting/ThrowExceptionTest.java
@@ -0,0 +1,42 @@
+package oop.overriting;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ThrowExceptionTest {
+}
+
+class SuperClassThrowsIOException {
+    void test() throws IOException {
+    }
+}
+
+class SubClassThrowsIOException extends SuperClassThrowsIOException {
+    @Override
+    void test() throws IOException {
+        System.out.println("sub class throws IOException");
+    }
+}
+
+class SubClassThrowsFileNotFoundException extends SuperClassThrowsIOException {
+    @Override
+    void test() throws FileNotFoundException {
+        System.out.println("sub class throws FileNotFoundException");
+    }
+}
+
+class SubClassThrowsNothing extends SuperClassThrowsIOException {
+    @Override
+    void test() {
+        System.out.println("sub class throws nothing");
+    }
+}
+
+
+class SubClassThrowsSQLException extends SuperClassThrowsIOException {
+    // Will not work, cannot throw any checked exception that is wider
+//    @Override
+//    void test() throws SQLException {
+//        System.out.println("sub class throws SQLException");
+//    }
+}
