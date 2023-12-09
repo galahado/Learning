24e47e805e605e1a48c2c090ce18a9e3895086b2
Chuanqi Zhang <zhchqi@gmail.com>
2023-12-09 15:51:58 -0500

    Servlet HttpServletRequestMethods

---
+++ Java/Servlet/HttpServletRequestTest/src/com/chuanqi/HttpServletRequestMethods.java
@@ -0,0 +1,45 @@
+package com.chuanqi;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HttpServletRequestMethods extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("HttpServletRequestMethods doPost() called");
+
+        // 1. 请求的统一资源定位符（绝对路径）URL
+        System.out.println("URL = " + req.getRequestURL());
+
+        // 2. 请求的客户端ip地址和端口
+        System.out.println("requester ip address = " + req.getRemoteAddr() + ":" + req.getRemotePort());
+
+        // 3. get the Referer form Header
+        System.out.println("The referer = " + req.getHeader("Referer"));
+
+        // 4. get parameter value username and pwd and solve Chinese encoding problem
+        req.setCharacterEncoding("utf-8");
+        System.out.println("username: " + req.getParameter("username"));
+        System.out.println("password: " + req.getParameter("pwd"));
+
+        // 5. get all the parameter values from hobby
+        String[] hobbies = req.getParameterValues("hobby");
+        for (String hobby : hobbies) {
+            System.out.println("hobby = " + hobby);
+        }
+
+        // 6. output the username back to the browser and solve Chinese encoding problem
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter writer = resp.getWriter();
+        writer.print("<h1>提交的用户名= " + req.getParameter("username") + "</h1>");
+    }
+}
---
+++ Java/Servlet/HttpServletRequestTest/src/com/exercise/HttpServletRequestMethods.java
@@ -0,0 +1,32 @@
+package com.exercise;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HttpServletRequestMethods extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("HttpServletRequestMethods doPost() called");
+
+        // 1. 请求的统一资源定位符（绝对路径）URL
+
+        // 2. 请求的客户端ip地址和端口
+
+        // 3. get the Referer form Header
+
+        // 4. get parameter value username and pwd and solve Chinese encoding problem
+
+        // 5. get all the parameter values from hobby
+
+        // 6. output the username back to the browser and solve Chinese encoding problem
+    }
+}
